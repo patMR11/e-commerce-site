9bb68e3967097cce4f3501038db68e138d753d77
patMR11 <patrickmucyo1109@gmail.com>
2025-12-06 12:58:51 +1000

    New changes

--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -29,14 +29,9 @@ function Products({setItems, sportCategory, sortOrder}) {
     const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
     useEffect(()=>{
-        fetch(`${process.env.PUBLIC_URL}/data.json`)
-            .then(res => {
-                if (!res.ok) throw new Error(`HTTP error! status: ${res.status}`);
-                return res.json();
-            })
-            .then(data => console.log(data))
-            .catch(err => console.error("Fetch error:", err));
-
+        fetch('data.JSON')
+            .then((response) => response.json())
+            .then((data) => setProduct(data))
     },[])
 
     useEffect(()=>{
--- src/main.jsx
+++ src/main.jsx
@@ -7,5 +7,5 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
     <HashRouter>
       <App />
-    </HashRouter>,
+    </HashRouter>
 )
