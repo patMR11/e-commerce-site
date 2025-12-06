77fb566f79d43012b51234563898a103388f75fc
patMR11 <patrickmucyo1109@gmail.com>
2025-12-06 11:56:08 +1000

    Prepare for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/e-commerce-site/', // ← your GitHub repo name
+  base: '/e-commerce-site/',   // IMPORTANT for GitHub Pages
 })
